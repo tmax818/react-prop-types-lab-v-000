1fec7deea1a8ebe14cea0678258643e96ccbb56f
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-11 05:31:26 -0700

    init commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,20 @@
-// Code Product Component Here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import propTypesRange from 'prop-types-range';
+
+export default class Product extends Component {
+  render() {
+    return <div />;
+  }
+}
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: propTypesRange(80, 300).isRequired
+};
